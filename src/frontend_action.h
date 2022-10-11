7842856efd784c282e2f46e6e3599e4c29904f2f
Henrique Preto <henrique.preto@tecnico.ulisboa.pt>
2022-10-11 23:04:00 +0100

    Add IgnoringDiagConsumer to Compiler instance to ignore some errors when running the cc ast tool

--- src/frontend_action.h
+++ src/frontend_action.h
@@ -5,6 +5,7 @@
 #include "clang/AST/ASTConsumer.h"
 #include "clang/Frontend/CompilerInstance.h"
 #include "clang/Frontend/FrontendAction.h"
+#include "clang/Basic/Diagnostic.h"
 #include "src/ast_consumer.h"
 
 template <typename T>
@@ -17,6 +18,7 @@ public:
 template <typename T>
 std::unique_ptr<clang::ASTConsumer> FrontendAction<T>::CreateASTConsumer(
     clang::CompilerInstance &Compiler, llvm::StringRef InFile) {
+  Compiler.getDiagnostics().setClient(new clang::IgnoringDiagConsumer(), /* ShouldOwnClient = */ true);
   return std::make_unique<ASTConsumer<T>>(&Compiler.getASTContext());
 }
 
