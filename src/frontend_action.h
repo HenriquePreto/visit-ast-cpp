7537ba1453625b5000662579dcbfaa6c3b0eb2c6
Henrique Preto <henrique.preto@tecnico.ulisboa.pt>
2022-10-21 14:28:34 +0100

    Refactor frontend action

--- src/frontend_action.h
+++ src/frontend_action.h
@@ -2,11 +2,13 @@
 #define CC_AST_TOOL_FRONTEND_ACTION_H_
 
 #include <memory>
+
+#include "src/ast_consumer.h"
+
 #include "clang/AST/ASTConsumer.h"
 #include "clang/Frontend/CompilerInstance.h"
 #include "clang/Frontend/FrontendAction.h"
 #include "clang/Basic/Diagnostic.h"
-#include "src/ast_consumer.h"
 
 template <typename T>
 class FrontendAction : public clang::ASTFrontendAction {
@@ -24,10 +26,10 @@ class FrontendAction : public clang::ASTFrontendAction {
 template <typename T>
 std::unique_ptr<clang::ASTConsumer> FrontendAction<T>::CreateASTConsumer(
     clang::CompilerInstance& instance, llvm::StringRef) {
-  instance.getDiagnostics().setClient(new clang::IgnoringDiagConsumer(), 
-                                      /* ShouldOwnClient = */ true);
-  return std::make_unique<ASTConsumer<T>>(instance.getASTContext(), 
-                                          visitor_info_);
+  instance.getDiagnostics().setClient(
+    new clang::IgnoringDiagConsumer(), /* ShouldOwnClient = */ true);
+  return std::make_unique<ASTConsumer<T>>(
+    instance.getASTContext(), visitor_info_);
 }
 
 #endif // CC_AST_TOOL_FRONTEND_ACTION_H_
